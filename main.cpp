cea7c5aec0c8503d38be7d7b38b0f82759ab1224
svandex <svandex@users.noreply.github.com>
2018-03-27 19:36:19 +0800

    controller input as main loop

--- main.cpp
+++ main.cpp
@@ -13,7 +13,25 @@ int main() {
 		XboxController::showBattery();
 
 		//get bluetooth device
+		auto mybluetoothdevice = BlueToothDevice::Instance();
+		mybluetoothdevice->on_initialise();
+
+		//initialise gamepad reading
+		GamepadReading oldReading = mygamepad->GetCurrentReading();
+		GamepadReading newReading = mygamepad->GetCurrentReading();
+
+		//loop
+		while (XboxController::isConnected) {
+			oldReading = newReading;
+			newReading = mygamepad->GetCurrentReading();
+			//if newReading != oldReading, start a new thread to do response
+			//make sure controller recognise your continous input
+			//make sure loop in main thread not block
+		}
 	}
+	//TWO SITUATIONS:
+	//1. xbox controller disconnected
+	//2. bluetooth device disconnected or communiation blocking
 	catch (std::exception e) {
 		std::cout << e.what() << std::endl;
 	}
